59eab58194c823433dd71dc63120d6c903b3a30b
debabrata-97 <debabrataszr@gmail.com>
2023-02-01 20:08:56 +0530

    first commit

---
+++ daycase.sh
@@ -0,0 +1,11 @@
+read -p "Enter a  number:" num
+case $num in
+        1) echo "Sunday";;
+        2) echo "Monday";;
+        3) echo "Tuesday";;
+        4) echo "Wednesday";;
+        5) echo "Thursday";;
+        6) echo "Friday";;
+        7) echo "Saturday";;
+        *) echo "not a valid day";;
+esac
