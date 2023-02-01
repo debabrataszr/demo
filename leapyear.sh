45da961734fa26fab3963f216a5adb0932e6fcc3
debabrata-97 <debabrataszr@gmail.com>
2023-02-01 21:59:41 +0530

    first commit

--- leapyear.sh
+++ leapyear.sh
@@ -4,7 +4,7 @@ read y
 a=$((y%4))
 b=$((y%100))
 c=$((y%400))
-if [ $a -eq 0 ] && [ $b -ne  0 ] && [  $c -eq 0 ]
+if [ $a -eq 0 ] && [ $b -eq  0 ] && [  $c -eq 0 ]
 then
 echo "$y is leap year"
 else
