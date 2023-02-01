8a5abbfe7cd1f6bfd360a6d60d7b92ea7a508121
debabrata-97 <debabrataszr@gmail.com>
2023-02-01 18:01:17 +0530

    first commit

---
+++ leapyear.sh
@@ -0,0 +1,13 @@
+
+echo -n "Enter year (YYYY): "
+read y
+a=$((y%4))
+b=$((y%100))
+c=$((y%400))
+if [ $a -eq 0 ] && [ $b -ne  0 ] && [  $c -eq 0 ]
+then
+echo "$y is leap year"
+else
+echo "$y is not a leap year"
+
+fi
